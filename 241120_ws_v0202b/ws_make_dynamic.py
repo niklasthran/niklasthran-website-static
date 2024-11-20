9d7a08599a28b86f3a9296f902f48b605b8f7e76
niklasthran <niklas.thran@me.com>
2024-11-20 17:16:24 +0100

    Style update in CSS and HTML structure

---
+++ 241120_ws_v0202b/ws_make_dynamic.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+import json
+import os
+data_location = "./static"
+
+# load project data from file
+json_data = json.load(open(f"{data_location}/project_data.json"))
+# sort data by date key, latest first
+json_data = dict(sorted(json_data.items(), key = lambda x: x[1]["date"], reverse=True))
+
+# generate images and video urls
+all_files = []
+for key in json_data.keys():
+    videos = []
+    images = []
+    all_files = os.listdir(f"{data_location}/{key}")
+    #all_files.remove(".DS_Store")
+    for file in all_files:
+        if ".mp4" in file or ".mov" in file:
+            videos.append(f"{data_location}/{key}/{file}")
+        if ".jpg" in file or ".png" in file:
+            images.append(f"{data_location}/{key}/{file}")
+        
+    json_data[key]["videos"] = videos
+    json_data[key]["images"] = images
+
+# List all photos in photo_archive directory
+photo_list = []
+for photo in os.listdir(f"{data_location}/photo_archive"):
+    if ".DS_Store" not in photo:
+        photo_list.append(f"{data_location}/photo_archive/{photo}")
+photo_list.sort(reverse=True)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", json_data=json_data, photo_list=photo_list)
+
+@app.route("/<key>.html")
+def new_project(key):
+
+    key_data = None
+
+    if key in json_data:
+        key_data = json_data[key]
+
+    return render_template("new_project.html", key_data=key_data)
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html", json_data=json_data)
+
+@app.route("/photo_archive.html")
+def photo_archive():
+    return render_template("photo_archive.html", photo_list=photo_list)
+
+if __name__ == "__main__":
+    app.run(debug = True)
