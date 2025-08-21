c63d5f57bda1e44b62401d76a9b9a1c19b892288
niklasthran <niklas.thran@me.com>
2025-08-21 15:26:07 +0200

    Content update

--- capsttest/script.js
+++ capsttest/script.js
@@ -2,70 +2,68 @@ import * as THREE from "three";
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 
 const container = document.querySelector("#container");
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-container.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-scene.background = new THREE.Color( 0xF2F3F4 );
+scene.background = new THREE.Color(0xF2F3F4);
 
 const ambient = new THREE.AmbientLight("white", 1.0);
 scene.add(ambient);
 
-const pointLight = new THREE.PointLight(0xffffff, 10000.0);
-pointLight.position.set(0, 30, 30);
-//scene.add(pointLight);
-
-//const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: false});
-
-
+/*
 const material = new THREE.MeshPhysicalMaterial(
 	{
-		roughness: 0.4,   
-		transmission: 1,  
-		thickness: 1,
-		clearcoat: 1,
+		roughness: 0.7,   
+		transmission: 1.0,  
+		thickness: 5,
+		ior: 1.3,
 
-	
+		color: Math.random() * 0xffffff,
+
 		transparent: true,
-		opacity: 0.5,
+		opacity: 0.85,
 		side: THREE.DoubleSide
 	}
 );
-
-/*
-const material = new THREE.MeshLambertMaterial(
-	{
-		color: Math.random() * 0xffffff
-	}
-);
 */
-const geometry = new THREE.BoxGeometry(30, 30, 1)
-let n_geos = 16;
+let n_geos = 128;
+let thick = 1;
+let dist = 1;
+const geometry = new THREE.BoxGeometry(30, 30, thick);
 for (let i = 0; i < n_geos; i++){
 	const cuboid = new THREE.Mesh(
 		geometry,
 		//material
+		
 		new THREE.MeshPhysicalMaterial(
 			{
-				roughness: 0.5,   
+				roughness: 0.7,   
 				transmission: 1.0,  
-				thickness: 4,
-				ior: 1.3,
-				//clearcoat: 1,
+				thickness: 6,
+				ior: 1.2,
 		
 				color: Math.random() * 0xffffff,
 
 				transparent: true,
-				opacity: 0.85,
+				opacity: 0.79,
 				side: THREE.DoubleSide
 			}
 		)
+		
 	);
 
-	cuboid.position.set(0, 0, (n_geos / 2 * -5) + i * 5);
+	cuboid.scale.set(
+		Math.random(10.0, 30.0),
+		Math.random(10.0, 30.0),
+		thick
+	);
+
+	cuboid.position.set(
+		0.0,
+		0.0, 
+		(- n_geos / 2 + (thick / 2)) * dist + i * dist
+	);
+
 	scene.add(cuboid);
 }
 
@@ -80,7 +78,7 @@ const camera = new THREE.OrthographicCamera(
 	1,
 	1000
 );
-//camera.position.set(-70, -20, -100);
+//camera.position.set(100, 0, 0);
 camera.position.set(70, 45, 100);
 camera.lookAt(0, 0, 0);
 
@@ -91,14 +89,19 @@ const gridHelper = new THREE.GridHelper( gsize, divisions );
 //scene.add( gridHelper );
 
 const axesHelper = new THREE.AxesHelper(50);
-//scene.add( axesHelper );
-
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 4);
-//scene.add(pointLightHelper);
+//scene.add(axesHelper);
 
 //const controls = new OrbitControls( camera, renderer.domElement );
 //controls.update();
 /////////
+
+const renderer = new THREE.WebGLRenderer(
+	{
+		antialias: true
+	}
+);
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 function onWindowResize() {
 	const newAspect = container.clientWidth / container.clientHeight;
@@ -114,6 +117,8 @@ function onWindowResize() {
 }
 
 renderer.render(scene, camera);
+container.appendChild(renderer.domElement);
+
 window.addEventListener("resize", onWindowResize);
 
 /*
